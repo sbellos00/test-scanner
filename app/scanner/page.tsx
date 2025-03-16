38e61e0493ec0781c7eb90c33736685db9516dc6
stefbellos00 <stefbellos00@gmail.com>
2025-03-16 13:33:48 +0200

    removed eslint

--- app/scanner/page.tsx
+++ app/scanner/page.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-namespace */
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
 import { useEffect, useRef } from 'react';
